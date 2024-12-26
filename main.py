1a8a6f249d8049be6e27f4113afdd14af06ab923
andrejabramov <andrzejvod@gmail.com>
2024-12-26 23:49:38 +0300

    20241226_todolist_flask1

--- main.py
+++ main.py
@@ -1,40 +1,20 @@
 from dataclasses import dataclass
-from flask import Flask, render_template, request, url_for, flash
-from werkzeug.utils import redirect
+from flask import Flask
+from flask_migrate import Migrate
+import config
+from models import db
+from views.todo_items import todo_app
+
 
 app = Flask(__name__)
 app.config.update(
-    SECRET_KEY="ytfhgjhjhjhjh465765"
+    SECRET_KEY="ytfhgjhjhjhjh465765",
+    SQLALCHEMY_DATABASE_URI=config.SQLITE_DB_URI,
 )
+db.init_app(app)
+migrate = Migrate(app, db=db)
 
-
-@dataclass
-class TodoItem:
-    text: str
-    done: bool = False
-
-
-todos = [
-    TodoItem("Learn Flask"),
-    TodoItem("Learn FastAPI"),
-    TodoItem("Learn Django"),
-]
-
-
-@app.get("/", endpoint="todos-list")
-def todo_list():
-    return render_template("index.html", todos=todos)
-
-@app.post("/")
-def todo_add():
-    text = request.form.get("todo-text")
-    if text:
-        todos.append(
-            TodoItem(text)
-        )
-    else:
-        flash("error, text not passed")
-    return redirect(url_for("todos-list"))
+app.register_blueprint(todo_app)
 
 
 if __name__ == '__main__':
---
+++ models/base.py
@@ -0,0 +1,7 @@
+# from typing import List, Optional
+# from sqlalchemy import false, ForeignKey, String
+# from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+#
+#
+# class Base(DeclarativeBase):
+#     id: Mapped[int] = mapped_column(primary_key=True)
